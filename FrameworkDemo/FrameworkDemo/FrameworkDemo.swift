5433b86457c6cbefe266dcaa52de0fdd5ae013a1
CSTNicole <000431652@student.vcc.ca>
2022-07-18 00:16:58 -0700

    added framework

---
+++ FrameworkDemo/FrameworkDemo/FrameworkDemo.swift
@@ -0,0 +1,14 @@
+//
+//  FrameworkDemo.swift
+//  FrameworkDemo
+//
+//  Created by Mary Nicolette Tan on 2022-07-18.
+//
+
+import Foundation
+
+public struct FrameworkDemo {
+    public static func sayHi(name: String) {
+        print("Good day to you \(name)!")
+    }
+}
